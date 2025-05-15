9f8a76833e06f1e478570730ca52fa8e95a77622
Tahira-Nawaz <tnawaz@puffersoft.com>
2025-05-15 16:57:24 +0500

    commit by tahira nawaz

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,14 @@
 <?php
 
-$servername="tahira-server.mysql.database.azure.com";
-$dbusername="tahira";
-$password="@bajwa123456789";
-$dBName="tahira-database";
+$servername = "tahira-server.mysql.database.azure.com";
+$dbusername = "tahira";
+$password = "@bajwa123456789";
+$dBName = "tahira-database";
 
-$conn=mysqli_connect($servername,$dbusername,$password,$dBName);
+$conn = mysqli_connect($servername, $dbusername, $password, $dBName);
 
-if(!$conn){
-	die("Connection failed: ".mysqli_connect_error());
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connection established successfully.";
 }
