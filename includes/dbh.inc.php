3d59fdf8e9946ee0fd0604e5f7a4c0e5422fea57
Tahira-Nawaz <tnawaz@puffersoft.com>
2025-05-16 16:02:49 +0500

    commit by tahira nawaz

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,16 +9,10 @@ $port = 3306;
 $ssl_ca = __DIR__ . "/certs/BaltimoreCyberTrustRoot.crt.pem"; // Path to SSL cert
 
 // Initialize connection with SSL
-$con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, $ssl_ca, NULL, NULL);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
 
 // Connect to database securely
-if (!mysqli_real_connect($con, $server, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($conn, $server, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("❌ Secure connection failed: " . mysqli_connect_error());
-    echo " Secure MySQL Connection not Successful!<br>";
-} 
-
-
-// else {
-//     echo "✅ Secure MySQL Connection Successful!<br>";
-// }
+}
